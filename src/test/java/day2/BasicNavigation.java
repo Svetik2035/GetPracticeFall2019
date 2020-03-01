195e14d36a830c59ecfbbc4ee89c5b54048616bb
LEGEN <legend3931@gmail.com>
2020-03-01 13:30:28 -0500

    Driver for chrome

---
+++ src/test/java/day2/BasicNavigation.java
@@ -0,0 +1,19 @@
+package day2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicNavigation {
+    public static void  main(String[] args){
+        //to start selenium script we need:
+        //setup webdriver (browser driver) and create webdriver object
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        // In Selenium, everything starts from WebDriver Interface
+        // you also can create an object like this: ChromeDriver driver = new ChromeDriver();
+
+        driver.get("http://google.com");//to open a website
+
+    }
+}
