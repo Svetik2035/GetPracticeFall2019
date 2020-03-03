5e8fe76a5f5b11ea7940fba46a43d23cbe84406f
LEGEN <legend3931@gmail.com>
2020-03-03 12:39:01 -0500

    Jumpfirst commit

---
+++ src/test/java/day2/QuitVsClose.java
@@ -0,0 +1,21 @@
+package day2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class QuitVsClose {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://practice.cybertekschool.com/open_new_tab");
+        Thread.sleep(5000);
+
+       // driver.close();//closes one window
+        driver.quit();// quit will closes all the tabs
+
+
+    }
+}
