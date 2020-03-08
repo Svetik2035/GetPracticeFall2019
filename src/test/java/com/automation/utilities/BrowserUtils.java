7447bedc52c9185cbfcadbc52ccba40e6083de4e
LEGEN <legend3931@gmail.com>
2020-03-08 11:42:46 -0400

    Practice Day5

---
+++ src/test/java/com/automation/utilities/BrowserUtils.java
@@ -0,0 +1,18 @@
+package com.automation.utilities;
+
+public class BrowserUtils {
+
+        public static void wait(int seconds){
+            try {
+                Thread.sleep(1000 * seconds);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+
+
+
